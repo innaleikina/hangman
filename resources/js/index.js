11933ceb6004e4f3be79c7ea83b68c86bd7523da
Inna <designbyinna@gmail.com>
2018-05-25 13:33:10 -0600

    right letters fill in blanks, wrong letters create an array

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,15 +3,16 @@ let i = 0;
 let newGame = document.getElementById('newGame');
 
 let titleToGuess = titlesArr[0];
+let titleToGuessArr = [];
 let underscores = document.getElementById("underscores");
 let underscoresArr = [];
 var guessesArr = [];
 let guesses = document.getElementById("guesses");
+let wrongGuesses = document.getElementById("wrongGuesses");
+var wrongGuessesArr;
 
 
 let currentUnderscores;
-
-
 
 
 //_____________________________________________GOES THROUGH SECRET WORD ARRAY____________
@@ -21,15 +22,15 @@ let currentUnderscores;
 newGame.addEventListener('click', function () {
   i = i + 1;
   i = i % titlesArr.length
-  titleToGuess = titlesArr[i];
+  titleToGuess = titlesArr[i].toLowerCase();
   titleLength = titleToGuess.length;
   console.log(titleToGuess);
   console.log(titleLength);
   newGame.innerHTML = "New Game";
   //resets the undersocres to 0
   underscoresArr = [];
+  //rsets guesses arr
   guessesArr = [];
-  wrongGuesses.innerHTML = guessesArr;
   makeUnderscores();
 });
 
@@ -39,6 +40,7 @@ newGame.addEventListener('click', function () {
 
 
 function makeUnderscores() {
+  titleToGuessArr
   for (var i = 0; i < titleToGuess.length; i++) {
     if (titleToGuess[i] === " ") {
       //adds multiple spaces to an array, so when the array is joined there is still visible space between multiple words
@@ -51,41 +53,58 @@ function makeUnderscores() {
   underscores.innerHTML = underscoresArr.join(" ");
 }
 
-//____________________________
-// function makeUnderscores(){
-//   currentUnderscores = titleToGuess.replace(/[a-zA-z]/g, "_");
-//   console.log (currentUnderscores);
-//   underscores.innerHTML = currentUnderscores;
-//   }
-//____________________________
-
 
 //_____________________________________________STORING USER GUESSES____________
-
+//for now works even before a game is started
+var keynum;
+function addToGuessArr(){
+  if (guessesArr.includes(String.fromCharCode(keynum))) {
+    console.log("letter has been used already")
+  } else {
+    guessesArr.push(String.fromCharCode(keynum));
+    //call the compare function on click
+    compare();
+}
+}
 
 function myKeyPress(e) {
-  var keynum;
 
  if (window.event) { // IE                    
     keynum = e.keyCode;
-    if (guessesArr.includes(String.fromCharCode(keynum))) {
-      console.log("letter has been used already")
-      //alert("You used this letter already. Try another one :) ");
-    } else {
-      guessesArr.push(String.fromCharCode(keynum));
+    addToGuessArr();
     }
-     wrongGuesses.innerHTML = guessesArr.join(" , ");
+
+
+
+    //duplicate code for netscape/firefox/opera
+     else if (e.which) { // Netscape/Firefox/Opera                   
+      keynum = e.which;
+      if (guessesArr.includes(String.fromCharCode(keynum))) {
+        console.log("letter has been used already")
+      } else {
+        guessesArr.push(String.fromCharCode(keynum));
+          //call the compare function on click
+         compare();
+      }
+
   }
-  else if(e.which){ // Netscape/Firefox/Opera                   
-    keynum = e.which;
-    
+}
+//_____________COMPARE LATEST INPUT WITH ORIGINAL ARRAY CHANGE BLANKS
+//_____________TO RIGHT GUESSES AND SHOW WRONG GUESSES ON THE SCREEN____________
+
+function compare() {
+  for (i = 0; i < titleToGuess.length; i++) {
+    if (titleToGuess.charAt(i) === guessesArr[guessesArr.length - 1]) {
+      underscoresArr.splice(i, 1, guessesArr[guessesArr.length - 1]);
+      underscores.innerHTML = underscoresArr.join(" ");
+      console.log("its a match!")
+    }
   }
 
-  console.log(guessesArr);
+  titleToGuessArr = titleToGuess.split("");
+  wrongGuessesArr = guessesArr.filter((word) => !titleToGuessArr.includes(word));
+  wrongGuesses.innerHTML = ("wrong guesses: " + wrongGuessesArr.join(" , "));
 }
 
-//_____________________________________________COMPARE INPUT WITH ORIGINAL ARRAY____________
 
-
-
-//_____________________________________________CHANGE BLANKS TO RIGHT GUESSES____________
+//_________________________________________________________
